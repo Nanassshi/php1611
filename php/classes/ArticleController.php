1776a9855de7b3d10e0e42df4706af103d8f6259
Nanashi{Polyakov Danil} <123826136+Nanassshi@users.noreply.github.com>
2023-02-11 17:47:02 +0300

    Вывод статей через json

--- php/classes/ArticleController.php
+++ php/classes/ArticleController.php
@@ -9,19 +9,7 @@ class ArticleController{
         global $mysqli;
         $result = $mysqli->query("SELECT * FROM articles WHERE id = $articleId");
         $row = $result->fetch_assoc();
-        $content = "
-            <div class='container-fluid bg-abstract py-4'>
-                <div class='col-sm-6 mx-auto py-5 transparent-bg px-3'>
-                    <h1 class='h-text'>".$row['title']."</h1>
-                    <div class='h-text'>".$row['content']."</div>
-                    <div class='h-text pt-3'>".$row['author']."</div>
-                    <div class='d-grid gap-2 d-md-flex justify-content-md-end'>
-                        <a href='/deleteArticle/".$row['id']."' class='btn my-3' style='background: #7d0805; color: #322c34'>Удалить</a>
-                    </div>
-                </div>
-            </div>";
-        return $content;
-        /*return json_encode($row);*/
+        return json_encode($row);
     }
 
     public static function deleteArticle($articleId){
